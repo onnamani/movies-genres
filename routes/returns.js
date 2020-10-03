bb4329ac404d5ba26b6b284e6326342712ce0073
Obinna <>
2020-10-03 17:54:08 +0100

    refactoring return route validation - created validateMiddleware.js

--- routes/returns.js
+++ routes/returns.js
@@ -1,14 +1,13 @@
 const moment = require('moment')
+const Joi = require('joi')
 const { Rental } = require('../models/rentalModel')
 const { Movies } = require('../models/movieModel')
+const validateMiddleware = require('../middleware/validateMiddleware')
 const auth = require('../middleware/authMiddleware')
 const express = require('express')
 const router = express.Router()
 
-router.post('/', auth, async (req, res) => {
-  if (!req.body.customerId) return res.status(400).send('customerId not found')
-  if (!req.body.movieId) return res.status(400).send('movieId not found')
-
+router.post('/', [auth, validateMiddleware(validate)], async (req, res) => { 
   let rental = await Rental.findOne({
     'customer._id': req.body.customerId,
     'movie._id': req.body.movieId
@@ -28,5 +27,14 @@ router.post('/', auth, async (req, res) => {
   
   return res.status(200).send(rental)
 })
+
+function validate(req) {
+  const schema = {
+    customerId: Joi.objectId().required(),
+    movieId: Joi.objectId().required()
+  };
+
+  return Joi.validate(req, schema);
+}
 
 module.exports = router
