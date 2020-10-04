982776b9beff61c7bcce7a11bbb4b5695fe86bab
Obinna <>
2020-10-04 14:29:29 +0100

    test case - rentals get request should return rentals object

---
+++ test/integration/rental.test.js
@@ -0,0 +1,36 @@
+const request = require('supertest')
+const { Rental } = require('../../models/rentalModel')
+
+jest.setTimeout(30000)
+
+describe('/api/rentals', () => {
+  let server
+
+  beforeEach(() => {
+    server = require('../../index')
+  })
+
+  afterEach(async () => {
+    await Rental.deleteMany({})
+    await server.close()
+  })
+
+  describe('GET /', () => {
+    it('should return all rentals', async () => {
+      await Rental.collection.insertMany(
+        [{
+          customer: { name: "Obinna Nnamani", phone: "0817000000" },
+          movie: { title: "Terminator", dailyRentalRate: 2 }
+        },
+        {
+          customer: { name: "Ogo Nnamani", phone: "0807000000" },
+          movie: { title: "Distraction", dailyRentalRate: 3 }
+        }]
+      )
+  
+      const res = await request(server).get('/api/rentals')
+  
+      expect(res.status).toBe(200)      
+    })
+  })
+})
