9f383ed2fa589b77c698c598d67f72943807eca2
Obinna <>
2020-10-04 10:43:01 +0100

    refactoring customer.test.js - cleaner code

--- test/integration/customer.test.js
+++ test/integration/customer.test.js
@@ -121,69 +121,80 @@ describe('/api/customers', () => {
   })  
 
   describe("PUT /:id", () => {
-    it("should return 404 if customer is not found", async () => {
-      const token = new User().generateAuthToken();
-      const customerId = mongoose.Types.ObjectId().toHexString();
+    let token
+    let customer
+    let customerId
 
-      const res = await request(server)
+    const exec = () => {
+      return request(server)
         .put("/api/customers/" + customerId)
         .set("x-auth-token", token)
         .send({ name: "Obinna", phone: "0807000000", isGold: true });
+    }
 
-      expect(res.status).toBe(404);
-    });
-
-    it("should return an updated customer", async () => {
-      const token = new User().generateAuthToken();
-      const customer = new Customer({
+    beforeEach(() => {
+      token = new User().generateAuthToken();
+       customer = new Customer({
         name: "Obinna Nnamani",
         phone: "0817000000"
       })
+    })
+    
+    it("should return 404 if customer is not found", async () => {
+      customerId = mongoose.Types.ObjectId().toHexString();   
+      
+      const res = await exec()
+      
+      expect(res.status).toBe(404);
+    });
+    
+    it("should return an updated customer", async () => {     
       await customer.save()
+      customerId = customer._id
 
-      const res = await request(server)
-        .put("/api/customers/" + customer._id)
-        .set("x-auth-token", token)
-        .send({ name: "Obinna", phone: "0807000000", isGold: true });
-
+      const res = await exec()
+      
       expect(res.status).toBe(200)
       expect(res.body).toHaveProperty('isGold', true)
     });
   });
 
   describe('Delete /:id', () => {
-    it('should return 404 if customer with given id is not found', async () => {
-      const user = new User({
-        _id: mongoose.Types.ObjectId(),
-        isAdmin: true
-      })
-      const token = user.generateAuthToken()
-      const customerId = mongoose.Types.ObjectId().toHexString()
+    let user
+    let token
+    let customer
+    let customerId
 
-      const res = await request(server)
-        .delete('/api/customers/' + customerId)
+    const exec = () => {
+      return request(server)
+        .delete('/api/customers/' + customer._id)
         .set('x-auth-token', token)
+    }
+
+    beforeEach(() => {
+      user = new User({
+        _id: mongoose.Types.ObjectId(),
+        isAdmin: true,
+      });
+      token = user.generateAuthToken();
+      customer = new Customer({
+        name: "Obinna Nnamani",
+        phone: "0817000000",
+      });
+    })
+
+    it('should return 404 if customer with given id is not found', async () => {      
+      customerId = mongoose.Types.ObjectId().toHexString()
+
+      const res = await exec()
       
       expect(res.status).toBe(404)
     })
 
     it('should delete customer', async () => {
-      const user = new User({
-        _id: mongoose.Types.ObjectId(),
-        isAdmin: true
-      })
-
-      const token = user.generateAuthToken()
-
-      const customer = new Customer({
-        name: "Obinna Nnamani",
-        phone: "0817000000"
-      })
       await customer.save()
 
-      const res = await request(server)
-        .delete('/api/customers/' + customer._id)
-        .set('x-auth-token', token)
+      const res = await exec()
       
       expect(res.status).toBe(200)
       expect(res.body).toHaveProperty("isGold", false)
